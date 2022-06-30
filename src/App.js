a6fed2d8567cd81ebd5ca0c35e0debca5192dce1
Dendeberia Oleksandr <thefangamchann@gmail.com>
2022-06-30 13:43:34 +0300

    Chapter 1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <main className={"app"}>
+      <Counter/>
+    </main>
+  );
+}
+
+export default App;
